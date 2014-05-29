eac846602a42986edb31778a202cd11de58c481b
phette23 <phette23@gmail.com>
2014-05-29 10:22:36 -0400

    add tab completion for "g" git alias

--- inc/completion.sh
+++ inc/completion.sh
@@ -16,3 +16,8 @@ if command -v "brew" &> /dev/null 2>&1; then
         source $(brew --prefix)/etc/bash_completion
     fi
 fi
+
+# Enable tab completion for `g` by marking it as an alias for `git`
+if type _git &> /dev/null && [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+    complete -o default -o nospace -F _git g
+fi
