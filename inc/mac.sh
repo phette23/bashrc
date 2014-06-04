891a846ef83cd63a7f92eb5cfa11585c69b097d7
phette23 <phette23@gmail.com>
2014-06-04 16:25:08 -0700

    mac: fix problem with tab() AppleScript

--- inc/mac.sh
+++ inc/mac.sh
@@ -119,11 +119,10 @@ EOF
   [[ "$the_app" == 'iTerm' ]] && {
     osascript 2>/dev/null <<EOF
       tell application "iTerm"
-        set current_terminal to current terminal
-        tell current_terminal
+        activate
+        tell the first terminal
           launch session "Default Session"
-          set current_session to current session
-          tell current_session
+          tell the last session
             write text "${command}"
           end tell
         end tell
