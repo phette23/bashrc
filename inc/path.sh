7ba4398f58fe6eef8ec6ab624bb0d2cd6dda64ee
phette23 <phette23@gmail.com>
2016-08-16 21:46:09 -0700

    rm ~/npm/bin from PATH, not needed with nvm

--- inc/path.sh
+++ inc/path.sh
@@ -1,7 +1,7 @@
 # 1) To make Homebrew work, put it at the front of the path
 # so brew packages are used over Mac OS defaults (e.g. git)
 # 2) global NPM packages (e.g. Grunt, Yeoman)
-export PATH="/usr/local/bin:${HOME}/npm/bin:/usr/local/pear/bin:${HOME}/bin:${PATH}"
+export PATH="/usr/local/bin:/usr/local/pear/bin:${HOME}/bin:${PATH}"
 # Add Gnu utilities if we have them in Homebrew
 if command -v brew &>/dev/null; then
     export PATH="$(brew --prefix coreutils)/libexec/gnubin:${PATH}"
